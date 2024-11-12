d74c617a73f6e10c0b6b1db0a288eb00c50f4667
Antonio <antonio92guerrerogarcia@gmail.com>
2024-11-12 19:26:20 +0100

    Final

--- Hilos/Supermercado.java
+++ Hilos/Supermercado.java
@@ -8,14 +8,12 @@ public class Supermercado {
     public static void main(String[] args) {
         long tiempoInicial = System.currentTimeMillis();
 
-        // Clientes con sus carritos
         String[] carritoCliente1 = {"Leche", "Pan", "Huevos", "Cereal", "Mantequilla"};
         String[] carritoCliente2 = {"Manzanas", "Azucar", "Arroz", "Pasta", "Tomates"};
         String[] carritoCliente3 = {"Cerveza", "Carne", "Queso", "Verduras", "Lechuga"};
         String[] carritoCliente4 = {"Galletas", "Refresco", "Cafe", "Sal", "Yogurt"};
         String[] carritoCliente5 = {"Papel Higienico", "Papel Aluminio", "Jabon", "Shampoo", "Detergente"};
 
-        // Crear hilos para representar los clientes
         Thread cliente1 = new Thread(() -> {
             lockCaja1.lock();
             try {
@@ -61,7 +59,6 @@ public class Supermercado {
             }
         });
 
-        // Iniciar los hilos
         cliente1.start();
         cliente2.start();
         cliente3.start();
